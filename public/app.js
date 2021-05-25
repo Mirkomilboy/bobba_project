d89e8f5aa532e5e36c53cbee1af993ececfad8bc
Mirkomilboy Sadirov <msadirov1997@gmail.com>
2021-05-25 14:59:53 +0500

    js functionalities

--- public/app.js
+++ public/app.js
@@ -11,82 +11,76 @@ menu.addEventListener('click', function () {
 const btn = document.querySelector('.btnWarning');
 
 btn.addEventListener('click', function () {
-    console.log('clicked');
-    document.querySelector('.warning').classList.add('hidden');
-    document.querySelector('.warning').classList.remove('flex');
+    document.querySelector('.warning').classList.toggle('hidden');
 });
 
 // =============== accordion ===============
 const menuLinks = document.querySelectorAll(".accordion .accordionBtn");
-menuLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    let menuContent = link.nextElementSibling;
-    let active = document.querySelector(".accordion .accordionMain.active");
 
-    if (menuContent) {
-          if (link.parentElement.classList.contains("active")) {
-            link.parentElement.classList.remove("active");
-            active.lastElementChild.style.height = "0";
-            link.querySelector("svg").style.transform = "rotate(0deg)";
-          } else {
-            link.parentElement.classList.add("active");
+menuLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        let menuContent = link.nextElementSibling;
+        let active = document.querySelector(".accordion .accordionMain.active");
+        
+        link.classList.toggle('active')
+        
+
+        if(link.classList.contains('active')){
             menuContent.style.height = menuContent.scrollHeight + "px";
             link.querySelector("svg").style.transform = "rotate(180deg)";
-          }
-      }
-  });
+        } else {
+            menuContent.style.height ="0";
+            link.querySelector("svg").style.transform = "rotate(0deg)";
+        }
+
+        // if (menuContent) {
+            // link.classList.toggle('active')
+
+            // if (link.parentElement.classList.contains("active")) {
+            //     link.parentElement.classList.remove("active");
+            //     active.lastElementChild.style.height = "0";
+            //     link.querySelector("svg").style.transform = "rotate(0deg)";
+            // } else {
+            //     link.parentElement.classList.add("active");
+            //     menuContent.style.height = menuContent.scrollHeight + "px";
+            //     link.querySelector("svg").style.transform = "rotate(180deg)";
+            // }
+        // }
+    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ========================== chart.js
-// const config = {
-//     type: 'line',
-//     data,
-//     options: {}
-//   };
+let myChart = document.getElementById('myChart').getContext('2d');
 
-//   const labels = [
-//     '01',
-//     '06',
-//     '11',
-//     '16',
-//     '21',
-//     '26',
-//     '31',
-//     '01',
-//   ];
-//   const data = {
-//     labels: labels,
-//     datasets: [{
-//       label: 'My First dataset',
-//       backgroundColor: 'rgb(255, 99, 132)',
-//       borderColor: 'rgb(255, 99, 132)',
-//       data: [10.000, 30.000, 60.000, 90.000, 120.000],
-//     }]
-//   };
-
-//   var myChart = new Chart(
-//     document.getElementById('myChart'),
-//     config
-//   );
+let LineChart = new Chart(myChart, {
+    type:'line',
+    data:{
+        labels:[
+            '01',
+            '06',
+            '11',
+            '16',
+            '21',
+            '26',
+            '31',
+            '01'
+        ],
+        datasets:[{
+            label:'Traffic',
+            data: [{x: '01', y: 10000}, {x: '06', y: 30000}, {x: '11', y: 50000}, {x: '16', y: 40000}, {x: '21', y: 90000}, {x: '26', y: 70000}, {x: '31', y: 80000}, {x: '01', y: 70000},],
+            borderColor: '#247FDA'
+        }],
+    },
+    options:{
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
